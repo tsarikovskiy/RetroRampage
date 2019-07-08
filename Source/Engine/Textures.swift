d336743c4d560edfe820d5ad689d56028165ae85
Nick Lockwood <nick@charcoaldesign.co.uk>
2019-07-08 07:18:37 +0100

    Part 4

---
+++ Source/Engine/Textures.swift
@@ -0,0 +1,34 @@
+//
+//  Textures.swift
+//  Engine
+//
+//  Created by Nick Lockwood on 05/06/2019.
+//  Copyright © 2019 Nick Lockwood. All rights reserved.
+//
+
+public enum Texture: String, CaseIterable {
+    case wall, wall2
+    case crackwall, crackwall2
+    case slimewall, slimewall2
+    case floor
+    case crackfloor
+    case ceiling
+}
+
+public struct Textures {
+    private let textures: [Texture: Bitmap]
+}
+
+public extension Textures {
+    init(loader: (String) -> Bitmap) {
+        var textures = [Texture: Bitmap]()
+        for texture in Texture.allCases {
+            textures[texture] = loader(texture.rawValue)
+        }
+        self.init(textures: textures)
+    }
+
+    subscript(_ texture: Texture) -> Bitmap {
+        return textures[texture]!
+    }
+}
--- Source/Engine/Tile.swift
+++ Source/Engine/Tile.swift
@@ -9,15 +9,33 @@
 public enum Tile: Int, Decodable {
     case floor
     case wall
+    case crackwall
+    case slimewall
+    case crackfloor
 }
 
 public extension Tile {
     var isWall: Bool {
         switch self {
-        case .wall:
+        case .wall, .crackwall, .slimewall:
             return true
-        case .floor:
+        case .floor, .crackfloor:
             return false
+        }
+    }
+
+    var textures: [Texture] {
+        switch self {
+        case .floor:
+            return [.floor, .ceiling]
+        case .crackfloor:
+            return [.crackfloor, .ceiling]
+        case .wall:
+            return [.wall, .wall2]
+        case .crackwall:
+            return [.crackwall, .crackwall2]
+        case .slimewall:
+            return [.slimewall, .slimewall2]
         }
     }
 }
